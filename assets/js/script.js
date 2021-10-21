4557be9ab3abb899d26c221bd2d5e09a6b85b250
David Silveira <daviewave@gmail.com>
2021-10-21 05:12:56 -0400

    updated script.js by adding a JQuery variable to target the timer in upper right hand corner of page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,14 @@
 //DEPENDENCIES
 var startQuizBtn = $("#start");
+var secondsRemaining = $("#seconds-left");
 
 //DATA
 
 //FUNCTIONS
-function startQuiz() {
-  console.log("harry buttholes");
-}
+//TODO: create function to start the quiz
+function startQuiz() {}
+
+function startTimer() {}
 
 //USER INTERACTIONS
 startQuizBtn.on("click", startQuiz);
