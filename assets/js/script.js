e6af1b9f959b063789c689ab421e93d4a9d8757f
David Silveira <daviewave@gmail.com>
2021-10-21 08:42:56 -0400

    updated script.js by adding variables to target desired elements in the newly structered index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,9 @@
 //DEPENDENCIES
-var startQuizBtn = $("#start");
+var startQuizBtn = document.getElementById("start");
+var quizCard = document.getElementById("quiz-card");
+var resultsCard = document.getElementById("quiz-card");
+var highscoreCard = document.getElementById("quiz-card");
 var secondsRemaining = document.getElementById("seconds-left");
-var question = document.getElementById("quiz-question");
-var answers = document.getElementById("intro-paragragh");
 
 //DATA
 //bool to track if the current screen is the intro screen (still press start button)
@@ -59,13 +60,15 @@ var quizQuestions = [
 //FUNCTIONS
 //TODO: create function to start the quiz
 function startQuiz() {
-  startQuizBtn.hide();
+  //changes button text to from start to submit and starts timer
+  startQuizBtn.innerHTML = "Submit Answer";
   startTimer();
 
   //1. Need to present the user the first question + answer options
-  for (var i = 0; i < quizQuestions.length; i++) {
-    // question.textContent = quizQuestions[i].question_string;
-  }
+  //   for (var i = 0; i < quizQuestions.length; i++) {
+  //     question.textContent = quizQuestions[i].question_string;
+  //     answers.textContent = quizQuestions[i].choices;
+  //   }
 
   //2.a Store quiz questions in an array and start looping array to display first question
   //2.b Store quiz questions in an array and start looping array to display first question
@@ -88,4 +91,4 @@ function startTimer() {
 }
 
 //USER INTERACTIONS
-startQuizBtn.on("click", startQuiz);
+startQuizBtn.addEventListener("click", startQuiz);
