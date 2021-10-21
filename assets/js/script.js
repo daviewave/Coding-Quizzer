dffffc41ab34c3b239e070822df476c6c6e3bb42
David Silveira <daviewave@gmail.com>
2021-10-21 07:04:30 -0400

    updated script.js by deleting the array containing just the quiz questions and replacing it with an array of objects containing but quiz questions and the possible answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,5 @@
 //DEPENDENCIES
 var startQuizBtn = $("#start");
-var secondsRemaining = $("");
-
 var secondsRemaining = document.getElementById("seconds-left");
 var question = document.getElementById("quiz-question");
 var answers = document.getElementById("intro-paragragh");
@@ -12,25 +10,67 @@ var isStartingScreen = true;
 
 //Need an array containing the quiz questions and potential answers
 var quizQuestions = [
-  "1. Which of the following is not a way to define a new variable in javascript?",
-  "2. Which of the following is not a primitive data type in javascript?",
-  "3. Which of the following data types returns a true/false value?",
-  "4. What is the name of a function that contains a parameter that accepts another function as an argument?",
-  "5. When using Git, which line of code will allow you to switch from the current branch to the main branch?",
+  {
+    question_string:
+      "1. Which of the following is not a way to define a new variable in javascript?",
+    choices: {
+      correct: "new",
+      wrong: ["var", "let", "const"],
+    },
+  },
+  {
+    question_string:
+      "2. Which of the following are not semantic elements in html5?",
+    choices: {
+      correct: "<middle>",
+      wrong: ["<article>", "<footer>", "<main>"],
+    },
+  },
+  {
+    question_string:
+      "3. Which of the following data types returns a true/false value?",
+    choices: {
+      correct: "boolean",
+      wrong: ["integer", "string", "float"],
+    },
+  },
+  {
+    question_string:
+      "4. What is the name of a function that contains a parameter that accepts another function as an argument?",
+    choices: {
+      correct: "Call-back function",
+      wrong: [
+        "Nested function",
+        "Recursive Function",
+        "function with-in a function",
+      ],
+    },
+  },
+  {
+    question_string:
+      "5. When using Git, which line of code will allow you to switch from the current branch to the main branch?",
+    choices: {
+      correct: "git checkout main",
+      wrong: ["git commit -m", "git add -A", "git init"],
+    },
+  },
 ];
 
 //FUNCTIONS
 //TODO: create function to start the quiz
 function startQuiz() {
+  startQuizBtn.disabled = true;
   startTimer();
 
-  /*1. Need to present the user the first question + answer options
-    
+  //1. Need to present the user the first question + answer options
+  for (var i = 0; i < quizQuestions.length; i++) {
+    // question.textContent = quizQuestions[i].question_string;
+  }
 
   //2.a Store quiz questions in an array and start looping array to display first question
   //2.b Store quiz questions in an array and start looping array to display first question
-  
-  //3. Handle when each button is pressed  */
+
+  //3. Handle when each button is pressed  //
 }
 
 function startTimer() {
