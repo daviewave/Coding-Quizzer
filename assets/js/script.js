1c0a56129c0f851b0c67a65e3d19e6aacc78d661
David Silveira <daviewave@gmail.com>
2021-10-21 06:22:14 -0400

    updated script.js by adding 2 variables that target the 'quiz-question' and 'intro-paragragh' elements within index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,29 @@
 //DEPENDENCIES
 var startQuizBtn = $("#start");
+var secondsRemaining = $("");
+
 var secondsRemaining = document.getElementById("seconds-left");
+//1.a target html element with id 'quiz-question' -- will replace title of quiz
+//1.b target html element with id 'intro-paragragh' -- will replace quiz description
+var question = document.getElementById("quiz-question");
+var answers = document.getElementById("intro-paragragh");
 
 //DATA
 //bool to track if the current screen is the intro screen (still press start button)
-var isStartingScreen;
+var isStartingScreen = true;
 
 //FUNCTIONS
 //TODO: create function to start the quiz
 function startQuiz() {
   startTimer();
 
-  //1. Need to present the user the first question + answer options
+  /*1. Need to present the user the first question + answer options
+    
 
-  //2. Handle when each button is pressed
+  //2.a Store quiz questions in an array and start looping array to display first question
+  //2.b Store quiz questions in an array and start looping array to display first question
+  
+  //3. Handle when each button is pressed  */
 }
 
 function startTimer() {
